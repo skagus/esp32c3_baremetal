12bd019e9016be7327c6a51615405c98fb4df910
Skagus Yun <skagus@gmail.com>
2023-05-01 20:23:30 +0900

    some bootcode changed

--- esp32c3/startup.c
+++ esp32c3/startup.c
@@ -15,14 +15,24 @@ extern unsigned char _vector_table;
 
 extern void _delay(int nCnt);
 
+#if 0
+extern void _reset();
+void __attribute__((section (".text.entry"))) start() 
+{
+    _reset();
+}
+#endif
+
+extern void mydelay(int nCnt);
+
 void __attribute__((naked)) startup()
 {
-    asm volatile("la gp, __global_pointer$");
+//    asm volatile("la gp, __global_pointer$");
     asm volatile("la sp, __stack_top");
-
     asm volatile("csrw mtvec, %0":: "r"((uint8_t *)(&_vector_table) + 1));
-#if 0
-    _delay(20000000);
+    printf("Start: %s\n", __FILE__);
+#if 1
+    mydelay(20000000);
     printf("DATA: %X, %X <-- %X, %X\n",
         &sdata, &edata, &_data_lma, *(unsigned int*)&_data_lma);
     printf("ZI: %X, %X\n", &sbss, &ebss);
